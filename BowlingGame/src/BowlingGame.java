aa302c366d95f64d4eb03c7b34e476994ba4c83b
krekicamina <akrekic1@etf.unsa.ba>
2017-11-25 19:23:13 +0100

    Lab assignment 1

--- BowlingGame/src/BowlingGame.java
+++ BowlingGame/src/BowlingGame.java
@@ -8,69 +8,123 @@ import java.util.ArrayList;
 public class BowlingGame {
 
 	String game;
-	ArrayList<Integer> scoresList;
+	ArrayList<Integer> frames;
 	 
 	public BowlingGame(String game)
 	{	
 		this.game = game;
-		this.scoresList = new ArrayList<Integer>();
-	}
-	
-	public String GetGame()
-	{
-		return this.game;
+		this.frames = new ArrayList<Integer>();
 	}
 	
 	public boolean checkGameFormat()
 	{
-		if (this.game.charAt(0) != '[' && this.game.charAt(this.game.length()-1)!=']')
+		if (this.game=="")
+			return false;
+		if ((this.game.charAt(0) != '[') && ((this.game.charAt(this.game.length()-1)) != ']'))
 			return false;
 		
 		String score = this.game.replace("][", "-");
-        score = score.replace("]", "");
-		score = score.replace("[", "");
 		
+        score = score.replace(",", "-");
+        
+        score = score.substring(1, score.length()-1);
+        System.out.println(score);
+        
         String[] numbersArray = score.split("-");
         
-        if(numbersArray.length<20 || numbersArray.length>22 )
+        if (numbersArray.length<20 || numbersArray.length>22)
             return false;
         else
         {
-        	for (int i=0; i<numbersArray.length;i++) {
+        	for (int i=0; i<numbersArray.length; i++) {
         		try
         		{
-        			Integer scoreint = Integer.parseInt(numbersArray[i]);
-        			if (scoreint<0||scoreint>10)
+        			Integer scoreInt = Integer.parseInt(numbersArray[i]);
+        			if (scoreInt<0 || scoreInt>10)
         				return false;
-        			this.scoresList.add(scoreint);
+        			this.frames.add(scoreInt);
         		}
-        		catch (NumberFormatException e){
+        		catch (NumberFormatException e)
+        		{
         			return false;
         		}
         	}	
         }
         
-        for (int i=0; i<scoresList.size()-1; i=i+2){
-        	if ((this.scoresList.get(i) + this.scoresList.get(i+1))>10)
+        if (this.frames.size()==20)
+        {
+        	for (int i=0; i<this.frames.size(); i=i+2)
+        	{
+        		if (((this.frames.get(i)+this.frames.get(i+1))<0) || ((this.frames.get(i)+this.frames.get(i+1))>10))
+        			return false;
+        		if (i==18) //spare in the last frame, bonus throw
+        		{
+        			if (this.frames.get(i)+this.frames.get(i+1)==10)
+						return false;
+        		}
+        	}
+        }
+        
+        if (this.frames.size()==21)
+        {
+        	if ((this.frames.get(this.frames.size()-2)+this.frames.get((this.frames.size()-3)) != 10) || (this.frames.get(this.frames.size()-3) == 10) || (this.frames.get(this.frames.size()-2) == 10))
         		return false;
         }
         
-        if(this.scoresList.size()==22) {
-            if(this.scoresList.get(this.scoresList.size()-4)!=10 && this.scoresList.get(this.scoresList.size()-3)!=0)
-            	return false;         
+        if (this.frames.size()==22) 
+        {
+        	if((this.frames.get(this.frames.size()-4))!=10)
+            {
+        		if((this.frames.get(this.frames.size()-3))!=10)
+            		return false;
+            }
+            if((this.frames.get(this.frames.size()-4))==10)
+            {
+            	if ((this.frames.get(this.frames.size()-3))!=0)
+            		return false;
+            }
+            if((this.frames.get(this.frames.size()-3))==10)
+            {
+            	if ((this.frames.get(this.frames.size()-4))!=0)
+            		return false;
+            }
         }
         
 		return true;
 	}
+	
 	
 	/** getScore method returns a score of current Bowling game or -1 if error
 	 * 
 	 * @return Integer value of Bowling score, in case of error return value is -1 
 	 */
 	
-	public int getScore() {
-		int TotalScore=-1;
-		return TotalScore;
+	public int getScore() 
+	{
+		int score=0;
+		if(!checkGameFormat())
+			return -1;
+		
+		for(int i=0; i<20; i=i+2)
+		{
+			if (this.frames.get(i)==10) //strike
+			{
+				if(this.frames.get(i+2)==10) //multiple strike
+				{
+					if(i!=18) //not last frame
+						score=score+this.frames.get(i)+this.frames.get(i+2)+this.frames.get(i+4);
+					else
+						score=score+this.frames.get(i)+this.frames.get(i+2)+this.frames.get(i+3);
+				}
+				else
+					score=score+this.frames.get(i)+this.frames.get(i+2)+this.frames.get(i+3);
+			} 
+			else if ((this.frames.get(i)+this.frames.get(i+1))==10) //spare
+				score=score+10+this.frames.get(i+2);
+			else //open
+				score=score+this.frames.get(i)+this.frames.get(i+1);
+		}
+		return score;
 	}
-	
 }
+
--- BowlingGame/src/BowlingGameTest.java
+++ BowlingGame/src/BowlingGameTest.java
@@ -14,22 +14,96 @@ public class BowlingGameTest extends TestCase {
 	/** test01 
 	 * 	
 	 *  If no game is provided, score should be -1 (error)   
-	 */	     
+	 */	  
+	BowlingGame bowlingGame = new BowlingGame("");
+	
 	public void test01() {
-        BowlingGame bowlingGame = new BowlingGame("");
         assertEquals(-1, bowlingGame.getScore());
     }	
 	public void testGame1() {
-        BowlingGame bowlingGame = new BowlingGame("1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
-        assertEquals(false, bowlingGame.checkGameFormat());
+        bowlingGame.game = ("[10,0][10,0][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(true, bowlingGame.checkGameFormat());
     }
 	public void testGame2() {
-        BowlingGame bowlingGame = new BowlingGame("[[10,0][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        bowlingGame.game = ("[10,0][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6],");
         assertEquals(false, bowlingGame.checkGameFormat());
     }	
 	public void testGame3() {
-        BowlingGame bowlingGame = new BowlingGame("[10,0][3.6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6][9,1]");
+        bowlingGame.game = ("[10,0][3.6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6][9,1]");
         assertEquals(false, bowlingGame.checkGameFormat());
     }
-
+	public void testGame4() {
+        bowlingGame.game = ("[8,2][5,5][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,5]");
+        assertEquals(true, bowlingGame.checkGameFormat());
+    }
+	public void testGame5() {
+        bowlingGame.game = ("[10,0][10,0][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,10]");
+        assertEquals(false, bowlingGame.checkGameFormat());
+    }
+	public void testGame6() {
+        bowlingGame.game = ("[1,5][3,6][7,2][2,5][4,4][5,3][3,3][4,5][8,1][2,8][7]");
+        assertEquals(true, bowlingGame.checkGameFormat());
+    }
+	public void testGame7() {
+        bowlingGame.game = ("[10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,10]");
+        assertEquals(true, bowlingGame.checkGameFormat());
+    }
+	public void testGame8() {
+        bowlingGame.game = ("[10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,10][10,10]");
+        assertEquals(false, bowlingGame.checkGameFormat());
+    }
+	public void testGame9() {
+        bowlingGame.game = ("[1,5][3,6][7,2][2,5][4,4][5,3][3,3][4,5][8,1][2,8][7,1]");
+        assertEquals(false, bowlingGame.checkGameFormat());
+	}
+	public void testGame10() {
+        bowlingGame.game = ("[1,5][3,6][7,2][2,5][4,4][5,3][3,3][4,5][8,1][2,8]");
+        assertEquals(false, bowlingGame.checkGameFormat());
+	}
+	public void testGame11() {
+        bowlingGame.game = ("[1,5][3,6][7,2][2,5][4,4][5,3][3,3][4,5][8,1][2,8][10]");
+        assertEquals(true, bowlingGame.checkGameFormat());
+    }
+	
+	public void testScore1() {
+        bowlingGame.game = ("[10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,10]");
+        assertEquals(300, bowlingGame.getScore());
+    }
+	public void testScore2() { 
+		bowlingGame.game = ("[8,2][5,5][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(98, bowlingGame.getScore());
+	}
+	public void testScore3() { 
+		bowlingGame.game = ("[10,0][4,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(103, bowlingGame.getScore());
+	}
+	public void testScore4() { 
+		bowlingGame.game = ("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,8][7]");
+		assertEquals(90, bowlingGame.getScore());
+	}
+	public void testScore5() {
+        bowlingGame.game = ("[10,0][10,0][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(112, bowlingGame.getScore());
+    }
+	public void testScore6() {
+        bowlingGame.game = ("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(81, bowlingGame.getScore());
+    }
+	public void testScore7() {
+        bowlingGame.game = ("[10,0][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(94, bowlingGame.getScore());
+    }
+	public void testScore8() {
+        bowlingGame.game = ("[1,9][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(88, bowlingGame.getScore());
+    }
+	public void testScore9() {
+        bowlingGame.game = ("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,8][10]");
+        assertEquals(93, bowlingGame.getScore());
+    }
+	public void testScore10() {
+        bowlingGame.game = ("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][10,0][7,2]");
+        assertEquals(92, bowlingGame.getScore());
+    }
+	
 }
